53fdacd7eea7d3287c53d53433170ea08002e90f
Ben Sklaroff <bsklaroff@gmail.com>
2014-06-02 11:03:27 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Arch Config
+My Arch Linux configuration for an Acer C720 Chromebook. I save it as
+
+    ~/.arch_config
+
+and then set up my system with
+
+    cd ~/.arch_config
+    ./setup.sh
+
+This installs some packages, soft links the config files to their proper
+locations, and starts some systemd services
+
+## What is all this stuff?
+- `paclist.txt` is a list of packages installed with pacman on my system,
+generated with the command `pacman -Qetn | cut -d' ' -f1`
+- `dotfiles` contains the hidden config files and folders in my home directory,
+such as .bashrc, .xinitrc, .xmonad, etc
+- `etc` contains the system config files found in `/etc` and its
+subdirectories, such as sound and touchpad settings
---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Install packages
+sudo pacman -Syu --needed $(< paclist.txt)
+
+echo 'Linking dotfiles...'
+# Soft link dotfiles into home directory
+for f in dotfiles/*
+do
+  ln -sf "$(pwd)/$f" "$HOME/.$(basename $f)"
+done
+
+# Remove annoying recursive soft links
+rm dotfiles/xmonad/xmonad
+rm dotfiles/vim/vim
+
+echo 'Linking etc...'
+# Soft link etc files into /etc directory
+for f in $(find etc -type f)
+do
+  sudo ln -sf "$(pwd)/$f" "/$f"
+done
+
+# Generate locale in /etc/locale.gen
+sudo locale-gen
+# Set localtime
+sudo ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
+# Set the hardware clock to UTC
+sudo hwclock --systohc --utc
+
+echo 'Starting systemd services...'
+### Systemd services
+# Automatically connect to WiFi
+sudo systemctl enable netctl-auto@wlp1s0
+sudo systemctl restart netctl-auto@wlp1s0
+# Sync date and time over the Internet
+sudo systemctl enable ntpd
+sudo systemctl restart ntpd
+
+echo 'Done!'
+
+# Patch touchpad kernel modules (only needed once)
+# wget http://pastie.org/pastes/9074242/download -O cros-haswell-modules-archlinux.sh
+# chmod +x cros-haswell-modules-archlinux.sh
+# ./cros-haswell-modules-archlinux.sh
