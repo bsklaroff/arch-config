36cc44302c1616d3f7d0a822b219813a353eff35
Ben Sklaroff <bsklaroff@gmail.com>
2014-07-17 11:22:35 -0700

    add keyboard shortcut to hide status bar

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,13 +1,16 @@
 import XMonad
-import XMonad.Hooks.ManageDocks(avoidStruts)
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers(doFullFloat,isFullscreen)
-import XMonad.Layout.NoBorders(Ambiguity(OnlyFloat),lessBorders)
+import XMonad.Layout.NoBorders(Ambiguity(OnlyFloat),smartBorders)
+import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run(spawnPipe)
 
 myStatusBar = "$HOME/.xmonad/dzheader"
 main = do
     statusBar <- spawnPipe myStatusBar
     xmonad $ defaultConfig
-      { layoutHook  = lessBorders OnlyFloat $ avoidStruts $ layoutHook defaultConfig
+      { layoutHook  = smartBorders $ avoidStruts $ layoutHook defaultConfig
       , manageHook = isFullscreen --> doFullFloat
       }
+      `additionalKeys`
+      [ ((mod1Mask, xK_b), sendMessage ToggleStruts) ]
